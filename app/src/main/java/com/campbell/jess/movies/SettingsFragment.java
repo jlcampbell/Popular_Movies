c2a07f50e520b7221965d0459aeb76a8990763eb
jlcampbell <jlcampbell@users.noreply.github.com>
2018-05-21 17:09:16 -0400

    settings fragments

--- app/src/main/java/com/campbell/jess/movies/SettingsFragment.java
+++ app/src/main/java/com/campbell/jess/movies/SettingsFragment.java
@@ -1,17 +1,44 @@
 package com.campbell.jess.movies;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
 //TODO finish settingsfragment class to complete settings UI
-public class SettingsFragment extends PreferenceFragment {
+//borrowed from sunshine project
+public class SettingsFragment extends PreferenceFragment implements
+        SharedPreferences.OnSharedPreferenceChangeListener {
+
+    private void setPreferenceSummary(Preference preference, Object value) {
+        String stringValue = value.toString();
+        String key = preference.getKey();
+
+        if (preference instanceof ListPreference) {
+            /* For list preferences, look up the correct display value in */
+            /* the preference's 'entries' list (since they have separate labels/values). */
+            ListPreference listPreference = (ListPreference) preference;
+            int prefIndex = listPreference.findIndexOfValue(stringValue);
+            if (prefIndex >= 0) {
+                preference.setSummary(listPreference.getEntries()[prefIndex]);
+            }
+        } else {
+            // For other preferences, set the summary to the value's simple string representation.
+            preference.setSummary(stringValue);
+        }
+    }
+
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void onCreate(Bundle savedInstanceState, String s) {
 
         //load preferences
         addPreferencesFromResource(R.xml.preferences);
     }
 
 
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String s) {
+
+    }
 }
